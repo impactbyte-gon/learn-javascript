722e4427fa3282c703dacb84c6278c9da47efbf0
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-01 13:40:03 +0700

    Implement display contacts and add contact

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log('Address Book')
 
+// -----------------------------------------------------------------------------
+// DATA
+
 let contacts = [
   {
     name: 'Haidar',
@@ -12,3 +15,32 @@ let contacts = [
     email: 'tony@stark.com'
   }
 ]
+
+// -----------------------------------------------------------------------------
+// PROGRAM
+
+const addressBook = {
+  displayContacts: function() {
+    contacts.forEach(contact => {
+      console.log(contact)
+    })
+  },
+
+  addContact: function(name, phone, email) {
+    console.log('ADD CONTACT')
+    console.log(name, phone, email)
+
+    contacts.push({
+      name,
+      phone,
+      email
+    })
+  }
+}
+
+// -----------------------------------------------------------------------------
+// RUN
+
+addressBook.displayContacts()
+addressBook.addContact('Steve', '+1-10101010', 'steve@rogers.com')
+addressBook.displayContacts()
